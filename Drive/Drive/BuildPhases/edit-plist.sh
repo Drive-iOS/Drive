a8a713713160650e18d67b2b4df39e5262434154
Aman Ketebo <amanketebo@yahoo.com>
2020-10-02 22:05:38 -0700

    Fixes failing builds

--- Drive/Drive/BuildPhases/edit-plist.sh
+++ Drive/Drive/BuildPhases/edit-plist.sh
@@ -1,7 +1,7 @@
 # Allow arbitrary loads only for `Debug Local` configuration
-/usr/libexec/PlistBuddy -c "Delete :NSAppTransportSecurity" "${INFOPLIST_FILE}"
 
-if [ "${CONFIGURATION}" = "Debug Local" ]; then
+/usr/libexec/PlistBuddy -c "Delete :NSAppTransportSecurity" "${INFOPLIST_FILE}"
+if [ "${CONFIGURATION}" = "Debug Localhost" ]; then
   /usr/libexec/PlistBuddy -c "Add:NSAppTransportSecurity:NSAllowsArbitraryLoads bool YES" "${INFOPLIST_FILE}"
 else
   /usr/libexec/PlistBuddy -c "Add:NSAppTransportSecurity:NSAllowsArbitraryLoads bool NO" "${INFOPLIST_FILE}"
